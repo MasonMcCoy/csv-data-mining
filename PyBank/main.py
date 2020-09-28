261c388efb6526aeab5e82b77494174759e225cb
Mason McCoy <mmasonmccoy@gmail.com>
2020-09-27 20:44:42 -0500

    Started work on PyBank Challenge, counted number of months. Started work on PyPoll.

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,3 +6,23 @@ Created on Sun Sep 27 19:09:48 2020
 """
 
 'Script for PyBank Python Challenge'
+
+import csv
+
+with open('C:/Users/Mason/Data_Bootcamp/Python_Challenge/PyBank/Resources/03-Python_HW_Instructions_PyBank_Resources_budget_data.csv', 'r') as csvfile:    
+    csvreader = csv.reader(csvfile, delimiter=',')
+    row_count = sum(1 for row in csvreader)
+    row_count = row_count - 1
+    print(row_count)
+    
+with open('C:/Users/Mason/Data_Bootcamp/Python_Challenge/PyBank/Resources/03-Python_HW_Instructions_PyBank_Resources_budget_data.csv', 'r') as csvfile:    
+    csvreader = csv.reader(csvfile, delimiter=',')   
+    net_charges = 0
+    for row in csvreader:
+        for column in row:
+            if column == 'Profit/Losses':
+                #net_charges += int(column)
+    #net_charges = sum(int(row[1])) for row in csvreader
+                print(net_charges)             
+                
+#net_charges/row_count
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,4 +5,14 @@ Created on Sun Sep 27 19:11:38 2020
 @author: Mason
 """
 
-'Script for PyPoll Python Challenge''
+'Script for PyPoll Python Challenge'
+
+import csv
+
+with open('C:/Users/Mason/Data_Bootcamp/Python_Challenge/PyPoll/Resources/03-Python_HW_Instructions_PyPoll_Resources_election_data.csv', 'r') as csvfile:
+    csvreader = csv.reader(csvfile)
+    row_count = sum(1 for row in csvreader)
+    row_count = row_count - 1
+    print(row_count)
+    
+    #returns 3521001?
