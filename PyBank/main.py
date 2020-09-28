ccdd1d1945403bb52ad6618648db197ac0ae3610
Mason McCoy <mmasonmccoy@gmail.com>
2020-09-27 19:12:56 -0500

    Added main.py files for both challenges.

---
+++ PyBank/main.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 27 19:09:48 2020
+
+@author: Mason
+"""
+
+'Script for PyBank Python Challenge'
---
+++ PyPoll/main.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 27 19:11:38 2020
+
+@author: Mason
+"""
+
+'Script for PyPoll Python Challenge''
