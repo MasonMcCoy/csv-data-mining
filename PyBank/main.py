cd6f29fb8e307a298bc6ea77d7ef375917a2a653
Mason McCoy <mmasonmccoy@gmail.com>
2020-09-28 20:02:04 -0500

    Added .txt file outputs for PyBank.py and PyPoll.py. Started work on second task for PyPoll.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,12 +9,14 @@ Created on Sun Sep 27 19:09:48 2020
 
 import csv
 
+#calculates the number of months in the dataset
 with open('C:/Users/Mason/Data_Bootcamp/Python_Challenge/PyBank/Resources/03-Python_HW_Instructions_PyBank_Resources_budget_data.csv', 'r') as csvfile:    
     csvreader = csv.reader(csvfile, delimiter=',')
-    row_count = sum(1 for row in csvreader)
-    row_count = row_count - 1
-    print(row_count)
+    total_months = sum(1 for row in csvreader)
+    total_months = total_months - 1
+    print(total_months)
     
+#calculates the sum of all charges
 with open('C:/Users/Mason/Data_Bootcamp/Python_Challenge/PyBank/Resources/03-Python_HW_Instructions_PyBank_Resources_budget_data.csv', 'r') as csvfile:    
     csvreader = csv.reader(csvfile, delimiter=',')   
     net_charges = 0
@@ -23,6 +25,29 @@ with open('C:/Users/Mason/Data_Bootcamp/Python_Challenge/PyBank/Resources/03-Pyt
             if column == 'Profit/Losses':
                 #net_charges += int(column)
     #net_charges = sum(int(row[1])) for row in csvreader
-                print(net_charges)             
+                print(net_charges)       
                 
-#net_charges/row_count
+#calculates average change of all charges
+#average_change = net_charges/row_count
+
+#prints analytic info to terminal
+print:('Financial Analysis')
+print:('----------------------------')
+print:('Total Months: ' + total_months)
+print:('Total: ' + net_charges)
+print:('Average Change: ')
+print:('Greatest Increase in Profits: ')
+print:('Greatest Decrease in Profits: ' )
+
+#Generates .txt file "results" with analyic info
+txt = open('bank_results.txt', 'wb')
+txt.write('Financial Analysis\n'.encode())
+txt.write('----------------------------\n'.encode())
+txt.write('Total Months: \n'.encode())
+txt.write('Total: \n'.encode())
+txt.write('Average Change: \n'.encode())
+txt.write('Greatest Increase in Profits: \n'.encode())
+txt.write('Greatest Decrease in Profits: '.encode())
+txt.close()
+    
+#How to print to correct folder?
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -14,5 +14,49 @@ with open('C:/Users/Mason/Data_Bootcamp/Python_Challenge/PyPoll/Resources/03-Pyt
     row_count = sum(1 for row in csvreader)
     row_count = row_count - 1
     print(row_count)
-    
     #returns 3521001?
+    candidates = []
+    for column in csvreader:
+        for row in column:
+            if column.header == 'Candidate' and row not in candidates:
+                    candidates.append(row)
+    print(candidates)
+    
+    #print:('Election Results')
+    #print:(-------------------------)
+    #print:('Total Votes: str(total_votes')
+    #print:(-------------------------)
+    #print:(candidate[1] + ": " + )
+    #print:(candidate[2] + ": " + )
+    #print:(candidate[3] + ": " + )
+    #print:(-------------------------)
+    #print:('Winner: ' + popular_winner)
+    #print:(-------------------------)
+
+    #prints analytic info to terminal
+    print:('Election Results')
+    print:('-------------------------')
+    print:('Total Votes: ')
+    print:('-------------------------')
+    print:('Khan: ')
+    print:('Correy: ')
+    print:('Li: ')
+    print:("O'Tooley: ")
+    print:('-------------------------')
+    print:('Winner: ')
+    print:('-------------------------')
+
+#Generates .txt file "poll_results" with voting info
+txt = open('poll_results.txt', 'wb')
+txt.write('Election Results'.encode())
+txt.write('----------------------------\n'.encode())
+txt.write('Total Votes: \n'.encode())
+txt.write('-------------------------\n'.encode())
+txt.write('Khan: \n'.encode())
+txt.write('Correy: \n'.encode())
+txt.write('Li: \n'.encode())
+txt.write("O'Tooley': \n".encode())
+txt.write('-------------------------\n'.encode())
+txt.write('Winner: \n'.encode())
+txt.write('-------------------------'.encode())
+txt.close()
